cac9898fd24a3ff6e1b620ea25de715a20ad9e0a
Tanner <35155694+TanmanG@users.noreply.github.com>
2025-04-09 01:28:19 -0700

    Removed unused imports.

--- PharmacyManager/PharmacyManager/Backend/DatabaseManager.cs
+++ PharmacyManager/PharmacyManager/Backend/DatabaseManager.cs
@@ -1,7 +1,6 @@
 ﻿using PharmacyManager.Backend.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Data.SqlClient;
 
 namespace PharmacyManager.Backend
--- PharmacyManager/PharmacyManager/Program.cs
+++ PharmacyManager/PharmacyManager/Program.cs
@@ -1,8 +1,6 @@
 ﻿using PharmacyManager.Backend;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PharmacyManager
